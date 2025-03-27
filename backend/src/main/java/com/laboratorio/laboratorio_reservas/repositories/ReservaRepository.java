867c74b003a451904ebcc9c4837e0c8f5389d060
Cristian <cristian.polo-g@mail.escuelaing.edu.co>
2025-03-27 17:22:36 -0500

    Actualizacion

--- backend/src/main/java/com/laboratorio/laboratorio_reservas/repositories/ReservaRepository.java
+++ backend/src/main/java/com/laboratorio/laboratorio_reservas/repositories/ReservaRepository.java
@@ -1,11 +1,12 @@
 package com.laboratorio.laboratorio_reservas.repositories;
 
-import com.laboratorio.laboratorio_reservas.models.Reserva;
+import java.util.Date;
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.Date;  // <-- Falta este import
-import java.util.List;  // <-- Falta este import
+import com.laboratorio.laboratorio_reservas.models.Reserva;
 
 @Repository
 public interface ReservaRepository extends MongoRepository<Reserva, String> {
